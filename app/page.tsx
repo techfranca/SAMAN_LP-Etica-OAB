2c06e93ca90de8a1ec24d72fa9c2b63b6027d2ca
Tech Franca <contato@francaassessoria.com>
2026-06-08 14:36:07 -0300

    update: app/page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -20,12 +20,12 @@ export default function Home() {
   return (
     <main className="min-h-screen bg-dark-950">
       <HeroSection checkoutUrl={CHECKOUT_URL} />
-      <ProblemSection />
-      <TurningPointSection />
-      <AuthoritySection />
-      <ContentSection />
-      <SocialProofSection />
-      <ForWhoSection />
+      <ProblemSection checkoutUrl={CHECKOUT_URL} />
+      <TurningPointSection checkoutUrl={CHECKOUT_URL} />
+      <AuthoritySection checkoutUrl={CHECKOUT_URL} />
+      <ContentSection checkoutUrl={CHECKOUT_URL} />
+      <SocialProofSection checkoutUrl={CHECKOUT_URL} />
+      <ForWhoSection checkoutUrl={CHECKOUT_URL} />
       <OfferSection checkoutUrl={CHECKOUT_URL} />
       <FaqSection />
       <FinalCtaSection checkoutUrl={CHECKOUT_URL} />
